d556077ccc53baf3fa043c8f28c55de1d03380fc
John Clary <john.clary@austintexas.gov>
2019-05-21 17:01:59 -0500

    update setup

--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="socratautil",
+    name="atd_socratautil",
     version="0.0.1",
     author="City of Austin",
     author_email="transportation.data@austintexas.gov",
     description="Utilities interacting with the Socrata Open Data API.",
-    install_requires=[
-        "requests",
-        "git+https://github.com/cityofaustin/atd-utils-data.git@master@egg=datautil-0"
-    ],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cityofaustin/atd-utils-socrata",
-    packages=setuptools.find_packages(),
+    install_requires = [
+        "atd_datautil",
+    ],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: Public Domain",
